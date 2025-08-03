bd3c4ef5677591bf1e607c34c3706b1142a3d97b
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-08-03 19:24:53 -0300

    equals, hashcode, == e IdentityHashMap

--- src/main/java/mx/florinda/cardapio/Main.java
+++ src/main/java/mx/florinda/cardapio/Main.java
@@ -7,17 +7,20 @@ public class Main {
     public static void main(String[] args) {
         Database database = new Database();
 
-        ItemCardapio item = database.itemCardapioPorId(1L).orElseThrow();
-        System.out.printf("'%s': R$ %.2f\n", item.nome(), item.preco());
+        ItemCardapio item = database.itemCardapioPorId(1L).orElseThrow(); // 2.99
 
-        boolean alterado1 = database.alteraPrecoItemCardapio(1L, new BigDecimal("3.99"));
-        System.out.printf("%s\n", alterado1 ? "Preço alterado 1" : "Não encontrado.");
+        database.alteraPrecoItemCardapio(1L, new BigDecimal("3.99")); // 2.99 => 3.99
+        ItemCardapio item1 = database.itemCardapioPorId(1L).orElseThrow(); // 3.99
 
-        boolean alterado2 = database.alteraPrecoItemCardapio(1L, new BigDecimal("2.99"));
-        System.out.printf("%s\n", alterado2 ? "Preço alterado 2" : "Não encontrado.");
+        database.alteraPrecoItemCardapio(1L, new BigDecimal("2.99")); // 3.99 => 2.99
+        ItemCardapio item2 = database.itemCardapioPorId(1L).orElseThrow(); // 2.99
 
-        boolean alterado3 = database.alteraPrecoItemCardapio(1L, new BigDecimal("4.99"));
-        System.out.printf("%s\n", alterado3 ? "Preço alterado 3" : "Não encontrado.");
+        database.alteraPrecoItemCardapio(1L, new BigDecimal("4.99")); // 2.99 => 4.99
+        ItemCardapio item3 = database.itemCardapioPorId(1L).orElseThrow(); // 4.99
+
+        System.out.println("== " + (item == item2));
+        System.out.println("equals() " + (item.equals(item2)));
+        System.out.println("hashCode() " + (item.hashCode() == item2.hashCode()));
 
         database.rastroAuditoriaPrecos();
     }
