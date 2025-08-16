3b4c506a36cbad0b3d27f14e56848dcb02184bba
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-08-16 17:55:33 -0300

    java.net.Socket

---
+++ src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
@@ -0,0 +1,50 @@
+package mx.florinda.cardapio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ServidorItensCardapioComSocket {
+
+    public static void main(String[] args) throws Exception {
+
+        try(ServerSocket serverSocket = new ServerSocket(8000)) {
+            System.out.println("Subiu servidor!");
+
+            while (true) {
+                try(Socket clientSocket = serverSocket.accept()) {
+                    InputStream clientIS = clientSocket.getInputStream();
+
+                    StringBuilder requestBuilder = new StringBuilder();
+                    int data;
+                    do {
+                        data = clientIS.read();
+                        requestBuilder.append((char) data);
+                    } while (clientIS.available() > 0);
+
+                    String request = requestBuilder.toString();
+                    System.out.println(request);
+
+                    Path path = Path.of("itensCardapio.json");
+                    String json = Files.readString(path);
+
+                    OutputStream clientOS = clientSocket.getOutputStream();
+                    PrintStream clientOut = new PrintStream(clientOS);
+
+                    clientOut.println("HTTP/1.1 200 OK");
+                    clientOut.println("Content-type: application/json; charset=UTF-8");
+                    clientOut.println();
+                    clientOut.println(json);
+                }
+            }
+
+        }
+
+    }
+
+}
