1150e1b8518f9c51f23b008f044aaf112e9757fe
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-08-17 19:58:22 -0300

    Threads

--- src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
+++ src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
@@ -1,6 +1,5 @@
 package mx.florinda.cardapio;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -17,36 +16,44 @@ public class ServidorItensCardapioComSocket {
             System.out.println("Subiu servidor!");
 
             while (true) {
-                try(Socket clientSocket = serverSocket.accept()) {
-                    InputStream clientIS = clientSocket.getInputStream();
-
-                    StringBuilder requestBuilder = new StringBuilder();
-                    int data;
-                    do {
-                        data = clientIS.read();
-                        requestBuilder.append((char) data);
-                    } while (clientIS.available() > 0);
-
-                    String request = requestBuilder.toString();
-                    System.out.println(request);
-
-                    Path path = Path.of("itensCardapio.json");
-                    String json = Files.readString(path);
-
-                    Thread.sleep(250);
-
-                    OutputStream clientOS = clientSocket.getOutputStream();
-                    PrintStream clientOut = new PrintStream(clientOS);
-
-                    clientOut.println("HTTP/1.1 200 OK");
-                    clientOut.println("Content-type: application/json; charset=UTF-8");
-                    clientOut.println();
-                    clientOut.println(json);
-                }
+                Socket clientSocket = serverSocket.accept();
+                new Thread(() -> trataRequisicao(clientSocket)).start();
             }
 
         }
+    }
 
+    private static void trataRequisicao(Socket clientSocket) {
+        try (clientSocket) {
+            InputStream clientIS = clientSocket.getInputStream();
+
+            StringBuilder requestBuilder = new StringBuilder();
+            int data;
+            do {
+                data = clientIS.read();
+                requestBuilder.append((char) data);
+            } while (clientIS.available() > 0);
+
+            String request = requestBuilder.toString();
+            System.out.println(request);
+
+            Thread.sleep(250);
+
+            Path path = Path.of("itensCardapio.json");
+            String json = Files.readString(path);
+
+                    Thread.sleep(250);
+
+            OutputStream clientOS = clientSocket.getOutputStream();
+            PrintStream clientOut = new PrintStream(clientOS);
+
+            clientOut.println("HTTP/1.1 200 OK");
+            clientOut.println("Content-type: application/json; charset=UTF-8");
+            clientOut.println();
+            clientOut.println(json);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
